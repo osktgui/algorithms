ea23ac70ee57890949386d13fc4c83f704de015c
Luis <luis@xurface.com>
2016-08-06 11:22:34 -0500

    first files

---
+++ 1ra/Dia 1/codigo/helloworld.py
@@ -0,0 +1,9 @@
+#esto es un comentario.
+precio = 19.3 #asigacion de una variable
+print (precio, type( precio ))
+
+precio = "Cadena" #asignacion de cadena
+print (precio, type(precio))
+
+logico = True
+print (logico, type(logico))
