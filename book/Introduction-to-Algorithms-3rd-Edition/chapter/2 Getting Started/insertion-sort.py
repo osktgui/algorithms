99e3f48533da33e6c67fef5d97aa59b02b7d1cb7
Rosalinda <Rosalinda>
2016-08-06 18:53:53 -0500

    Solution inseertion-sort

--- book/Introduction-to-Algorithms-3rd-Edition/chapter/2 Getting Started/insertion-sort.py
+++ book/Introduction-to-Algorithms-3rd-Edition/chapter/2 Getting Started/insertion-sort.py
@@ -1,1 +1,10 @@
-print ("Hello world!! - ok")
+lista = [5, 2, 4, 6, 1, 3]
+print(lista)
+for j in range(1,len(lista)):
+    key = lista[j]
+    i = j-1
+    while i >= 0 and lista[i] > key:
+        lista[i+1]=lista[i]
+        i= i-1
+    lista[i+1] = key
+print(lista)
