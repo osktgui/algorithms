d7cd19861fda2fe26d98729a6e0968cf2cb6e48e
Luis <luis@xurface.com>
2016-08-06 19:09:32 -0500

    files

---
+++ book/Introduction-to-Algorithms-3rd-Edition/chapters/2 Getting Started/Excersises/2.1-2.py
@@ -0,0 +1,10 @@
+lista = [5, 2, 4, 6, 1, 3]
+print(lista)
+for j in range(1,len(lista)):
+    key = lista[j]
+    i = j-1
+    while i >= 0 and lista[i] < key:
+        lista[i+1]=lista[i]
+        i= i-1
+    lista[i+1] = key
+print(lista)
